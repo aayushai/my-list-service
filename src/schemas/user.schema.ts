575ae92ad6c5cc82968e892d07806da0ec0f1325
aayushai <aayushhp17@gmail.com>
2024-07-03 02:44:16 +0530

    Changes

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,15 +1,37 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type UserDocument = User & Document;
+type Genre = 'Action' | 'Comedy' | 'Drama' | 'Fantasy' | 'Horror' | 'Romance' | 'SciFi';
 
 @Schema()
-export class User {
-  @Prop({ required: true, unique: true })
-  username: string;
+export class User extends Document {
+  @Prop({ required: true })
+  id: string;
 
   @Prop({ required: true })
-  password: string;
+  username: string;
+
+  @Prop({
+    favoriteGenres: [String],
+    dislikedGenres: [String],
+  })
+  preferences: {
+    favoriteGenres: Genre[];
+    dislikedGenres: Genre[];
+  };
+
+  @Prop({
+    watchHistory: [{
+      contentId: String,
+      watchedOn: Date,
+      rating: Number,
+    }]
+  })
+  watchHistory: Array<{
+    contentId: string;
+    watchedOn: Date;
+    rating?: number;
+  }>;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
